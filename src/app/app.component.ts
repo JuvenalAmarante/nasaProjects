4eb84e786efe9e584a5f3a05a9d4fc2df99e93d9
Juvenal Amarante <juvenalamarante13@gmail.com>
2023-04-13 10:05:01 -0300

    refactor: move layout structure to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'nasaProjects';
+  src = `https://upload.wikimedia.org/wikipedia/commons/thumb/e/e5/NASA_logo.svg/2449px-NASA_logo.svg.png`
+  placeholder = `${this.src}?x-oss-process=image/blur,r_50,s_50/quality,q_1'`;
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,8 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
-  srcLogo = `https://upload.wikimedia.org/wikipedia/commons/thumb/e/e5/NASA_logo.svg/2449px-NASA_logo.svg.png`
-  placeholderLogo = `${this.srcLogo}?x-oss-process=image/blur,r_50,s_50/quality,q_1'`;
   src = `https://graphics.jifo.co/imgs/d7534dd0ab4580aab4bec75873065fa317a0fb64fd3971e4cdcd5b6b394099b9.jpeg`;
   placeholder = `${this.src}?x-oss-process=image/blur,r_50,s_50/quality,q_1'`;
 }
